505454ffe5da31c29827a4975dcea497ac6343ba
Carlos Sarmiento <c.sarmientobaca@gmail.com>
2023-02-07 17:09:23 +0100

    added exercises

---
+++ src/components/AllTheBooks.jsx
@@ -0,0 +1,34 @@
+import { Col } from "react-bootstrap";
+import Card from 'react-bootstrap/Card';
+
+import Books from "../data/fantasy.json"
+import CardGroup from 'react-bootstrap/CardGroup';
+
+
+function CardMain() {
+    return (
+
+        Books.map(book => {
+            return (
+                <Col xs={12} md={4}>
+                    < Card >
+                        <Card.Img variant="top" src={book.img} />
+                        <Card.Body>
+                            <Card.Title>{book.title}</Card.Title>
+                            <Card.Text>
+                                {book.price}
+                            </Card.Text>
+                        </Card.Body>
+                    </Card >
+                </Col>
+            )
+        })
+    );
+}
+
+
+
+
+
+export default CardMain;
+// export default GroupExample;
